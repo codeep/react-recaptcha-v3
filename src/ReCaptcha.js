6f0d0c320c6fe176ed859cb6a2e097e3dfa92a1d
Ilgaz <ilgzcnn@gmail.com>
2019-04-10 07:28:23 +0300

    Added new feature

--- src/ReCaptcha.js
+++ src/ReCaptcha.js
@@ -1,96 +1,72 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
-
-const propTypes = {
-  elementID: PropTypes.string,
-  verifyCallbackName: PropTypes.string,
-  verifyCallback: PropTypes.func,
-  sitekey: PropTypes.string.isRequired,
-  action: PropTypes.string.isRequired
-}
-
-const defaultProps = {
-  elementID: 'g-recaptcha',
-  verifyCallbackName: 'verifyCallback'
-}
-
-const isReady = () =>
-  typeof window !== 'undefined' &&
-  typeof window.grecaptcha !== 'undefined' &&
-  typeof window.grecaptcha.execute !== 'undefined'
 
 let readyCheck
 
-class ReCaptcha extends Component {
-  constructor (props) {
-    super(props)
-
-    this.execute = this.execute.bind(this)
-
-    this.state = {
-      ready: isReady()
-    }
-
-    if (!this.state.ready) {
-      readyCheck = setInterval(this._updateReadyState.bind(this), 1000)
-    }
+export default function ReCaptcha (props) {
+  const isReady = () =>
+    typeof window !== 'undefined' &&
+    typeof window.grecaptcha !== 'undefined' &&
+    typeof window.grecaptcha.execute !== 'undefined'
+  
+  const [isCaptchaReady, setIsCaptchaReady] = useState(() => isReady())
+  useReadyChecker(isCaptchaReady, props)
+  if (!isCaptchaReady) {
+    readyCheck = setInterval(_updateReadyState, 1000)
   }
-
-  componentDidMount () {
-    if (this.state.ready) {
-      this.execute()
+  function _updateReadyState () {
+    if (isReady()) {
+      setIsCaptchaReady(true)
+      clearInterval(readyCheck)
     }
+    return (
+      isCaptchaReady ? (
+        <div
+          id={props.elementID}
+          data-verifycallbackname={props.verifyCallbackName}
+        />
+      ) : (
+        <div id={props.elementID} className='g-recaptcha' />
+      )
+    )
   }
+}
 
-  componentDidUpdate (_, prevState) {
-    if (this.state.ready && !prevState.ready) {
-      this.execute()
-    }
-  }
-
-  componentWillUnmount () {
-    clearInterval(readyCheck)
-  }
-
-  execute () {
+function useReadyChecker (readyState) {
+  function execute (props) {
     const {
       sitekey,
       verifyCallback,
       action,
-    } = this.props
-
-    if (this.state.ready) {
+    } = props
+  
+    if (readyState) {
       window.grecaptcha.execute(sitekey, { action })
         .then(token => {
-
           if (typeof verifyCallback !== 'undefined') {
             verifyCallback(token)
           }
         })
     }
   }
-
-  _updateReadyState () {
-    if (isReady()) {
-      this.setState(() => ({ ready: true }))
-
+  useEffect(() => {
+    if (readyState) {
+      execute()
+    }
+    return () => {
       clearInterval(readyCheck)
     }
-  }
-
-  render () {
-    return this.state.ready ? (
-      <div
-        id={this.props.elementID}
-        data-verifycallbackname={this.props.verifyCallbackName}
-      />
-    ) : (
-      <div id={this.props.elementID} className='g-recaptcha' />
-    )
-  }
+  }, [readyState])
 }
 
-ReCaptcha.propTypes = propTypes
-ReCaptcha.defaultProps = defaultProps
-
-export default ReCaptcha
+ReCaptcha.propTypes = {
+  elementID: PropTypes.string,
+  verifyCallbackName: PropTypes.string,
+  verifyCallback: PropTypes.func,
+  sitekey: PropTypes.string.isRequired,
+  action: PropTypes.string.isRequired
+}
+ReCaptcha.defaultProps = {
+  elementID: 'g-recaptcha',
+  verifyCallbackName: 'verifyCallback'
+}
