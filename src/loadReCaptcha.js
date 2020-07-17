86d301a2411a9aca09d2acb1fd9d6acc33ed70af
chaitanyapotti <chaitanya.potti@gmail.com>
2020-07-17 13:44:10 +0800

    Add support for async and defer

--- src/loadReCaptcha.js
+++ src/loadReCaptcha.js
@@ -1,11 +1,14 @@
 // Callback by default is an empty function to execute nothing when no callback is passed
-const loadReCaptcha = (siteKey, callback = () => {}) => {
+const loadReCaptcha = (siteKey, callback = () => {}, async = false, defer = false) => {
   const script = document.createElement('script')
 
   if (!window.onLoadCaptchaV3Callback && callback) {
     window.onLoadCaptchaV3Callback = callback
   }
   script.src = `https://www.recaptcha.net/recaptcha/api.js?onload=onLoadCaptchaV3Callback&render=${siteKey}`
+
+  script.async = async
+  script.defer = defer
 
   document.body.appendChild(script)
 }
